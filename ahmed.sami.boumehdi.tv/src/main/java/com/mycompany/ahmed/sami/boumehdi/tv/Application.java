a0b8c3167faf3c8ca12264f97a6e6b0c8b62142c
Sami <blabla@hotmail.fr>
2015-05-16 11:25:25 +0200

    Création du projet.

---
+++ ahmed.sami.boumehdi.tv/src/main/java/com/mycompany/ahmed/sami/boumehdi/tv/Application.java
@@ -0,0 +1,13 @@
+package com.mycompany.ahmed.sami.boumehdi.tv;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
