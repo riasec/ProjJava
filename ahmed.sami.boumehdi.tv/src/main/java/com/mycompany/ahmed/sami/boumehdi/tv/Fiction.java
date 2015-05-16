29a66c56dfff1e4bf04edebdf3bf3124f4a79d23
Sami <blabla@hotmail.fr>
2015-05-16 11:50:54 +0200

    Création de la classe Fiction (fille de Programme). Ajout des attributs, des imports et initialisation du logger.

---
+++ ahmed.sami.boumehdi.tv/src/main/java/com/mycompany/ahmed/sami/boumehdi/tv/Fiction.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ahmed.sami.boumehdi.tv;
+
+/**
+ *
+ * @author Ahmed-Sami
+ */
+import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Fiction extends Programme {
+  private int annee;
+  private String realisateur;
+  private boolean rediffusion;
+  private static final Logger logger = LoggerFactory.getLogger(Fiction.class);
+  
+}
