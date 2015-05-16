2b28ea299735aaf35125cb481caddacec45c1cc5
Sami <blabla@hotmail.fr>
2015-05-16 11:53:34 +0200

    Création de la classe Reportage (fille de Programme). Ajout des attributs, des imports et initialisation du logger.

---
+++ ahmed.sami.boumehdi.tv/src/main/java/com/mycompany/ahmed/sami/boumehdi/tv/Reportage.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ahmed.sami.boumehdi.tv;
+
+import java.util.StringTokenizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Ahmed-Sami
+ */
+public class Reportage extends Programme {
+   
+    private String theme;
+    private static final Logger logger = LoggerFactory.getLogger(Reportage.class);
+    
+}
