011913afab5306c23daa4a73ce657033137dc159
Sami <blabla@hotmail.fr>
2015-05-16 11:56:34 +0200

    Classe Fiction : Ajout d'un constructeur et des getter/setter

--- ahmed.sami.boumehdi.tv/src/main/java/com/mycompany/ahmed/sami/boumehdi/tv/Fiction.java
+++ ahmed.sami.boumehdi.tv/src/main/java/com/mycompany/ahmed/sami/boumehdi/tv/Fiction.java
@@ -14,9 +14,41 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Fiction extends Programme {
-  private int annee;
-  private String realisateur;
-  private boolean rediffusion;
-  private static final Logger logger = LoggerFactory.getLogger(Fiction.class);
-  
+    private int annee;
+    private String realisateur;
+    private boolean rediffusion;
+    private static final Logger logger = LoggerFactory.getLogger(Fiction.class);
+    public Fiction(int heure, int duree, String nom){
+       this.type="Fiction";
+       this.heure=heure;
+       this.duree=duree;
+       this.nom=nom;
+    }
+
+     public int getAnnee() {
+         return annee;
+     }
+     public void setAnnee(int annee) {
+        this.annee = annee;
+     }
+     public String getRealisateur() {
+         return realisateur;
+     }
+     public void setRealisateur(String realisateur) {
+        this.realisateur = realisateur;
+     }
+     public boolean getRediffusion() {
+         return rediffusion;
+     }
+     public void setRediffusion(boolean rediffusion) {
+        this.rediffusion = rediffusion;
+     }
+     public int getDuree()
+     {
+         return duree;
+     }
+     public int getDebut()
+     {
+         return heure;
+     }
 }
