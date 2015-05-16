963161c8710773bf43022a598693f5648032b967
Sami <blabla@hotmail.fr>
2015-05-16 12:02:58 +0200

    Application : ajout d'un arraylist à vérifier

--- ahmed.sami.boumehdi.tv/src/main/java/com/mycompany/ahmed/sami/boumehdi/tv/Application.java
+++ ahmed.sami.boumehdi.tv/src/main/java/com/mycompany/ahmed/sami/boumehdi/tv/Application.java
@@ -10,6 +10,17 @@ public class Application
     public static void main( String[] args )
     {
         System.out.println( "ProjetTele - Boumehdi Ahmed-Sami - Groupe C" );
+        /*********************************************************************
+                                ARRAYLIST A VERIFIER
+        *********************************************************************/
+        ArrayList<String> programmeList = new ArrayList<>();
+        programmeList.add("fiction, 21, 2, Database, 1970, Codd, non");
+        programmeList.add("divertissement, 18, TPMP, Cyril");
+        programmeList.add("reportage, 4, 1, Les perruches d'Alaska, animalier");
+        programmeList.add("FICTION, 10, 2, A.I., 2001, Steven Spielberg, OUI");
+        programmeList.add("reportage, 2, 1, test2, monde");
+        programmeList.add("reportage, 0, 1, test, culturel");
+        logger.info("La liste de programmes a tester a ete creee");
         
     }
 }
