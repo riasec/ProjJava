9e422949a4be0d1bc9e21205255fde1afa35b576
Sami <blabla@hotmail.fr>
2015-05-16 12:00:20 +0200

    Application : ajout des imports, initialisation du logger

--- ahmed.sami.boumehdi.tv/src/main/java/com/mycompany/ahmed/sami/boumehdi/tv/Application.java
+++ ahmed.sami.boumehdi.tv/src/main/java/com/mycompany/ahmed/sami/boumehdi/tv/Application.java
@@ -1,13 +1,15 @@
 package com.mycompany.ahmed.sami.boumehdi.tv;
 
-/**
- * Hello world!
- *
- */
+import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application
 {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "ProjetTele - Boumehdi Ahmed-Sami - Groupe C" );
+        
     }
 }
