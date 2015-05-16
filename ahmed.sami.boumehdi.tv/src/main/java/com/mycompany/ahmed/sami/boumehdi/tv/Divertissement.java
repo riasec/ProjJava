d0d2ee0da0c8690df0a2a4342eef833bbcf7bec9
Sami <blabla@hotmail.fr>
2015-05-16 11:52:23 +0200

    Création de la classe Divertissement (fille de Programme). Ajout des attributs, des imports et initialisation du logger.

---
+++ ahmed.sami.boumehdi.tv/src/main/java/com/mycompany/ahmed/sami/boumehdi/tv/Divertissement.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ahmed.sami.boumehdi.tv;
+
+import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Ahmed-Sami
+ */
+public class Divertissement extends Programme {
+    
+    private String animateur;
+    private static final Logger logger = LoggerFactory.getLogger(Divertissement.class);
+    
+}
